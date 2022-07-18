30e1ce5c2b6625c01278430e9e25b9c735eb04d3
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 18:29:12 +0700

    tugas 2 js
    untuk Niomic

---
+++ Tugas2/Tugas2.js
@@ -0,0 +1,15 @@
+var number = 21;
+console.log(number);
+var string = 'bilangan';
+console.log(string);
+var array = [3,4,5,6,7];
+console.log(array);
+var kosong;
+console.log(kosong);
+var boolean = false;
+boolean = 34;
+if(boolean<35)
+boolean = true;
+else
+boolean = false;
+console.log(boolean);
